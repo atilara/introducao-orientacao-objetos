015d45636e963999a792c6283a58b22e0f2ee15b
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-01-05 17:16:12 -0300

    fix: using variable instead of ArrayList to count the amount of bets

--- src/questoesAvancadas/megaSena/MinhaLoteria.java
+++ src/questoesAvancadas/megaSena/MinhaLoteria.java
@@ -8,17 +8,26 @@ public class MinhaLoteria {
         // Instância do bilhete premiado
         BilheteLoteria bilhetePremiado = new BilheteLoteria(6);
 
-        // Instância do ArrayList que irá armazenar os bilhetes
-        ArrayList<BilheteLoteria> bilhetes = new ArrayList<>();
+        // Vars auxiliares
+        boolean ganhou = false;
+        int quantQuadras = 0;
+        int quantQuinas = 0;
 
         // Laço Externo, que varia de 6 a 15
         int i;
         for (i = 6; i <= 15; i++) {
 
-            // Variáveis auxiliares
-            boolean ganhou = false;
-            int quantQuadras = 0;
-            int quantQuinas = 0;
+            // Zerando valor das vars auxiliares
+            ganhou = false;
+            quantQuadras = 0;
+            quantQuinas = 0;
+
+            // Instância do ArrayList que iria armazenar os bilhetes, comentado devido a memória
+            // ArrayList<BilheteLoteria> bilhetes = new ArrayList<>();
+
+            // Quantidade de apostas feitas em cada quant de numeros, substituindo o arraylist devido a
+            // problemas na memória
+            int quantApostas = 0;
 
             // Enquanto ganhou for falso, o laço interno continuará rodando
             while (!ganhou){
@@ -33,14 +42,17 @@ public class MinhaLoteria {
                     quantQuadras++;
                 }
 
-                // Aumento da quantidade de apostas e adição dos bilhetes no ArrayList
-                bilhetes.add(bilheteSorteado);
+                // Adição dos bilhetes no ArrayList comentado devido a problemas na memória
+                // bilhetes.add(bilheteSorteado);
+                // Utilizando variável aux para contar
+                quantApostas++;
             }
-            System.out.println("\nJogos de " + i + " números: você apostou " + bilhetes.size() + 1 + " vezes" +
+            // Utilizando var em vez de bilhetes.size para contar a quant de apostas, devido a memória
+            System.out.println("\nJogos de " + i + " números: você apostou " + quantApostas + " vezes" +
                     " para ganhar na mega-sena. Dentre esses jogos, você acertou " + quantQuadras +
                     " quadras e " + quantQuinas + " quinas.");
-            // Limpeza do ArrayList
-            bilhetes.removeAll(bilhetes);
+            // Limpeza do ArrayList caso fosse utilizado
+            // bilhetes.clear(bilhetes);
 
         }
     }
