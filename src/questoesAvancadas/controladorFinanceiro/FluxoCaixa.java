6c33249cf1db5f60e5ad3fb14d847396e767276d
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-01-06 16:18:20 -0300

    feat: create instances in main and tested the program

--- src/questoesAvancadas/controladorFinanceiro/FluxoCaixa.java
+++ src/questoesAvancadas/controladorFinanceiro/FluxoCaixa.java
@@ -1,18 +1,53 @@
 package questoesAvancadas.controladorFinanceiro;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class FluxoCaixa {
+
+    private final ArrayList<Transacao> transacoes;
 
     private String nomeEmpresa;
 
     private String cnpj;
 
-    void adicionarTransacao (Transacao transacao) {}
+    public FluxoCaixa(String nomeEmpresa, String cnpj) {
+        transacoes = new ArrayList<>();
+        this.nomeEmpresa = nomeEmpresa;
+        this.cnpj = cnpj;
+    }
+
+    public String getNomeEmpresa() {
+        return nomeEmpresa;
+    }
+
+    public void setNomeEmpresa(String nomeEmpresa) {
+        this.nomeEmpresa = nomeEmpresa;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    void adicionarTransacao (Transacao transacao) {
+        transacoes.add(transacao);
+    }
 
     double calcularDespesas (LocalDate inicio, LocalDate fim) {
-        return 1;
+        double valorDespesas = 0;
+        LocalDateTime inicioTime = inicio.atTime(0, 0);
+        LocalDateTime fimTime = fim.atTime(23, 59);
+        for (Transacao t : transacoes) {
+            if (t.getData().isAfter(inicioTime) && t.getData().isBefore(fimTime)){
+                valorDespesas += t.calcularTotal();
+            }
+        }
+        return valorDespesas;
     }
 
     double calcularReceitas (LocalDate inicio, LocalDate fim) {
--- src/questoesAvancadas/controladorFinanceiro/TesteControladorFinanceiro.java
+++ src/questoesAvancadas/controladorFinanceiro/TesteControladorFinanceiro.java
@@ -1,9 +1,52 @@
 package questoesAvancadas.controladorFinanceiro;
 
+import questoesAvancadas.controladorFinanceiro.despesas.DespesaComAgua;
+import questoesAvancadas.controladorFinanceiro.despesas.DespesaComEnergia;
+import questoesAvancadas.controladorFinanceiro.despesas.DespesaComInternet;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class TesteControladorFinanceiro {
 
     public static void main(String[] args) {
-        System.out.println("Main");
+
+        DespesaComAgua a = new DespesaComAgua(
+                LocalDateTime.of(2014, 12, 10, 12, 10),
+                "Conta de dezembro de 2014",
+                27
+        );
+
+        DespesaComInternet i = new DespesaComInternet(
+                LocalDateTime.of(2018, 12, 10, 12, 10),
+                "Conta de dezembro de 2014",
+                27
+        );
+
+        DespesaComEnergia e = new DespesaComEnergia(
+                LocalDateTime.of(2017, 12, 10, 12, 10),
+                "Conta de dezembro de 2014",
+                27
+        );
+
+        System.out.println(a.calcularTotal());
+
+        System.out.println(i.calcularTotal());
+
+        System.out.println(e.calcularTotal());
+
+        FluxoCaixa f = new FluxoCaixa("Blitzwolf", "0000-0");
+
+        f.adicionarTransacao(a);
+        f.adicionarTransacao(i);
+        f.adicionarTransacao(e);
+
+        System.out.println(
+                f.calcularDespesas(
+                        LocalDate.of(2014, 10, 10),
+                        LocalDate.of(2014, 12, 20)
+                )
+        );
     }
 
 }
