12b741387235bb2ecb1d352f64f01f6d802dcd8e
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-01-08 11:51:06 -0300

    refactor: modify ifs in the internet calculation

--- src/questoesAvancadas/controladorFinanceiro/despesas/DespesaComInternet.java
+++ src/questoesAvancadas/controladorFinanceiro/despesas/DespesaComInternet.java
@@ -25,11 +25,9 @@ public class DespesaComInternet extends Despesa {
         double valorFinal = 0;
 
         if (dadosTrafegados <= 20) valorFinal = 90.9;
-        else if (dadosTrafegados > 20 && dadosTrafegados < 100) valorFinal = 90.9 + ((dadosTrafegados - 20) * 3.9);
-        else if (dadosTrafegados == 100) valorFinal = 240.9;
-        else if (dadosTrafegados > 100 && dadosTrafegados < 500) valorFinal = 240.9  + ((dadosTrafegados - 100) * 4.4);
-        else if (dadosTrafegados == 500) valorFinal = 590.9;
-        else if (dadosTrafegados > 500) valorFinal = 590.9 + ((dadosTrafegados - 500) * 5.1);
+        else if (dadosTrafegados < 100) valorFinal = 90.9 + ((dadosTrafegados - 20) * 3.9);
+        else if (dadosTrafegados >= 100) valorFinal = 240.9  + ((dadosTrafegados - 100) * 4.4);
+        else if (dadosTrafegados >= 500) valorFinal = 590.9 + ((dadosTrafegados - 500) * 5.1);
 
         return valorFinal;
     }
